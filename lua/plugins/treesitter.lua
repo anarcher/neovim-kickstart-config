78c77ff6fcd24a52db28579e92ef4f619d1fa401
anarcher <anarcher@gmail.com>
2025-02-01 00:09:36 +0900

    feat(treesitter): add jsonnet to treesitter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -34,6 +34,7 @@ return { -- Highlight, edit, and navigate code
       'c',
       'rust',
       'zig',
+      'jsonnet',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
