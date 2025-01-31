277f4745f696801a3a170e839c8919a371789a65
anarcher <anarcher@gmail.com>
2025-01-31 23:22:19 +0900

    feat: Add support for C, Rust, and Zig in Treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,6 +31,9 @@ return { -- Highlight, edit, and navigate code
       'tsx',
       'css',
       'html',
+      'c',
+      'rust',
+      'zig',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
