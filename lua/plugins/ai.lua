e2e547836474fc07c59a03d622e8bf16fc36e4a0
anarcher <anarcher@gmail.com>
2025-01-31 23:15:13 +0900

    feat: Update plugin dependencies and configurations: vista,gopher,nvim.ai Ensure gofmt in none-ls

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,13 @@
+return {
+  'magicalne/nvim.ai',
+  version = '*',
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('ai').setup {
+      provider = 'anthropic',
+    }
+  end,
+}
