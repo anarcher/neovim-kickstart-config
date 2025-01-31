74dcb739871b2ff842636843e4bc61afb4244781
anarcher <anarcher@gmail.com>
2025-01-31 23:15:13 +0900

    feat: Update plugin dependencies and configurations: vista,gopher,nvim.ai Ensure gofmt in none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -18,6 +18,7 @@ return {
         'shfmt', -- Shell formatter
         'checkmake', -- linter for Makefiles
         'ruff', -- Python linter and formatter
+        'gofumpt', -- Go formatter
       },
       automatic_installation = true,
     }
@@ -28,6 +29,7 @@ return {
       formatting.stylua,
       formatting.shfmt.with { args = { '-i', '4' } },
       formatting.terraform_fmt,
+      formatting.gofumpt,
       require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
       require 'none-ls.formatting.ruff_format',
     }
