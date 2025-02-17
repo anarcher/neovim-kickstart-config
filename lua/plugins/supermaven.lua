4e89b97ff41170d420d70cf03f05393ea4ed47e6
anarcher <anarcher@gmail.com>
2025-02-17 17:01:30 +0900

    feat(plugins): autocompletion: Add SuperMaven and Codeium as autocompletion options

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,6 +1,14 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup {}
+    require('supermaven-nvim').setup {
+      condition = function()
+        -- Only works in buffer
+        -- return (vim.api.nvim_win_get_config(0).relative ~= '')
+
+        -- expect in nvim.ai chat-dialog
+        return (vim.bo.filetype == 'chat-dialog')
+      end,
+    }
   end,
 }
