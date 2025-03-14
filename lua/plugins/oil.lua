4b33e2aa7ebf7e54644853f5bc156f4a6e15eeef
anarcher <anarcher@gmail.com>
2025-03-14 11:30:35 +0900

    feat(plugins): add oil.nvim configuration with float mode

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,4 +8,16 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+
+  config = function()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
+    -- keymap
+    vim.keymap.set('n', '-', function()
+      require('oil').open_float()
+    end, { desc = 'Open oil in float mode' })
+  end,
 }
