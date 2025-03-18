a66a6eb6d44a0eb748123873c085eced06e7f38e
anarcher <anarcher@gmail.com>
2025-03-18 11:27:16 +0900

    feat(plugins): treesitter-context: config: max_lines = 3

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -9,6 +9,7 @@ return {
       -- enable = true, -- 기본값은 true입니다
       -- max_lines = 0, -- 컨텍스트가 표시될 최대 라인 수 (0 = 무제한)
       -- trim_scope = 'outer', -- 어떤 컨텍스트를 보여줄지 (inner/outer)
+      max_lines = 3,
     }
   end,
 }
