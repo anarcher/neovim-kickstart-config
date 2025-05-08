de9394a605e385439ffbd0474695bf0193cd4bc5
anarcher <anarcher@gmail.com>
2025-05-08 18:46:30 +0900

    feat(plugins): supermaven: keymaps accept_suggestion <S-Tab>

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -2,6 +2,9 @@ return {
   'supermaven-inc/supermaven-nvim',
   config = function()
     require('supermaven-nvim').setup {
+      keymaps = {
+        accept_suggestion = '<S-Tab>',
+      },
       condition = function()
         -- Only works in buffer
         -- return (vim.api.nvim_win_get_config(0).relative ~= '')
