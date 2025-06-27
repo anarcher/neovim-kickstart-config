c3bad393c601a58bf21e1f1d45644f39f540596a
anarcher <anarcher@gmail.com>
2025-06-27 16:03:25 +0900

    feat: add fzf.vim dependency to vim-go plugin

--- lua/plugins/vim-go.lua
+++ lua/plugins/vim-go.lua
@@ -1,6 +1,10 @@
 return {
   'fatih/vim-go',
   ft = { 'go' },
+  dependencies = {
+    'junegunn/fzf',
+    'junegunn/fzf.vim'
+  },
   config = function()
     vim.g.go_bin_path = os.getenv 'HOME' .. '/gotools'
   end,
